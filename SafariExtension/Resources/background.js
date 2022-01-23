5e29d7f286acb1de625d6d4eed65e48432b8b325
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2022-01-23 12:12:38 +0900

    SafariExtensionとネイティブの連携

--- SafariExtension/Resources/background.js
+++ SafariExtension/Resources/background.js
@@ -1,6 +1,18 @@
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
-
-    if (request.greeting === "hello")
-        sendResponse({ farewell: "goodbye" });
+    if (request.type != null) {
+        browser.runtime.sendNativeMessage("application.id", {message: "Hello from background page"}, function(response) {
+            if (response) {
+                const obj = JSON.parse(response);
+                if (obj.type) {
+                    sendResponse(obj);
+                }
+                
+            }
+            
+        });
+        
+    }
+    return true;
 });
+
--- SafariExtension/Resources/content.js
+++ SafariExtension/Resources/content.js
@@ -1,7 +1,48 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+function replaceEngineerToBlank(node) {
+    switch (node.nodeType)
+    {
+        case Node.ELEMENT_NODE:
+            if (node.tagName.toLowerCase() === "input" || node.tagName.toLowerCase() === "textarea") {
+                return;
+            }
+        case Node.DOCUMENT_NODE:
+        case Node.DOCUMENT_FRAGMENT_NODE:
+            var child = node.firstChild;
+            var next = undefined;
+            while (child) {
+                next = child.nextSibling;
+                replaceEngineerToBlank(child);
+                child = next;
+            }
+            break;
+        case Node.TEXT_NODE:
+            replaceTextInTextNode(node);
+            break;
+    }
+}
+ 
+function replaceTextInTextNode(textNode) {
+    if (textNode.nodeType !== Node.TEXT_NODE)
+        return;
+    
+    if (!textNode.nodeValue.length)
+        return;
+    
+    var enginner = new RegExp("エンジニア", "gi");
+    var nodeValue = textNode.nodeValue;
+    var newNodeValue = nodeValue.replace(enginner, "");
+    
+    if (nodeValue !== newNodeValue) {
+        textNode.nodeValue = newNodeValue;
+    }
+}
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+browser.runtime.sendMessage({
+    type: "content"
+},
+    function(response) {
+//    replaceEngineerToBlank(document.body);
+    if (response.type) {
+        replaceEngineerToBlank(document.body);
+    }
 });
