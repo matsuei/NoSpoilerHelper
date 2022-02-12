afa38655c1a6e8b19c5e8ff4aa4457cdbe2790df
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2022-02-12 21:54:46 +0900

    追加された時に使い方モーダルを表示

--- NoSpoilerHelper/HowToUseView.swift
+++ NoSpoilerHelper/HowToUseView.swift
@@ -9,9 +9,23 @@ import SwiftUI
 import AVKit
 
 struct HowToUseView: View {
+    @Environment(\.presentationMode) private var presentationMode
+    
     var body: some View {
-        VideoPlayer(player: AVPlayer(url:  Bundle.main.url(forResource: "tutorial", withExtension: "mov")!))
-            .frame(height: 400)
+        VStack {
+            VideoPlayer(player: AVPlayer(url:  Bundle.main.url(forResource: "tutorial", withExtension: "mov")!))
+                .frame(width: 200, height: 420, alignment: .center)
+            Text("Settings > Safari > Extension \nturn on NoSpolierHelper")
+                .font(.title3)
+                .lineSpacing(10)
+            Button(action: {
+                withAnimation {
+                    presentationMode.wrappedValue.dismiss()
+                }
+            }, label: {
+                Text("Close")
+            })
+        }
     }
 }
 
